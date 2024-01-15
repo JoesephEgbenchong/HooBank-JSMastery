04846ec05cf2fd8a052651b65d8c4efe50f51459
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-01-15 17:58:59 +0100

    Installed Router and created home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-center mt-10 text-gray-950 hover:text-yellow-300 cursor-pointer font-semibold">Hoo Bank website is gonna be awesome. Trust me</h1> 
+      <Router>
+        <Routes>
+          <Route path="/" element={ <Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+        <h1 className='font-bold'>The canvas awaits the artists</h1>
+    </>
+  )
+}
