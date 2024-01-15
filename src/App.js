3c7ca5f9f8fcbd7f74e75606ac35d925ea6e4a86
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-01-15 17:45:17 +0100

    Installed reactjs and tailwindcss

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center mt-10 text-gray-950 hover:text-yellow-300 cursor-pointer font-semibold">Hoo Bank website is gonna be awesome. Trust me</h1> 
+    </>
+  );
+}
+
+export default App;
