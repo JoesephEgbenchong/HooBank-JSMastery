3768728b9fc7732e082f52fb18cb435183d2b334
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-01-16 01:01:42 +0100

    Added assets, styles and constants from JSMastery

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import styles from '../styles';
 
 export default function Home() {
   return (
     <>
-        <div className=''>
-            <h1 className='text-center text-3xl text-red-400 text-bold mt-20'>Hello World</h1>
+        <div className='bg-primary w-full overflow-hidden'>
+            <div className={`${styles.paddingX} ${styles.flexCenter} `}>
+              <div className={`${styles.boxWidth}`}>
+                NavBar
+              </div>
+            </div>
+
+            <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+              <div className={`${styles.boxWidth}`}>
+                Hero
+              </div>
+            </div>
+
+            <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+              <div className={`${styles.boxWidth}`}>
+                Stats
+                Business
+                Billing
+                CardDeal
+                Testimonials
+                Clients
+                CTA
+                Footer
+              </div>
+            </div>
+
         </div>
     </>
   )
