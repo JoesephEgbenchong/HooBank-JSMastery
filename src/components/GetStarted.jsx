c27eba26857df469ecae14b823d8784d35d80f18
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-01-18 11:58:40 +0100

    Implemented left part of Hero Section

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import styles from '../styles'
+import { arrowUp } from '../assets'
 
 export default function GetStarted() {
   return (
-    <div>GetStarted</div>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] bg-blue-gradient rounded-full p-[2px] cursor-pointer`}>
+      <div className={`${styles.flexCenter} flex-col w-[100%] h-[100%] bg-primary rounded-full`}>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className='flex font-poppins font-medium text-[18px] leading-[23px]'>
+            <span className='text-gradient mr-2'>Get</span>
+            <img src={arrowUp} alt='arrow up' className='w-[23px] h-[23px] object-contain' />
+          </p>
+        </div>
+        <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+            <span className='text-gradient'>Started</span>
+        </p>
+      </div>
+    </div>
   )
 }
